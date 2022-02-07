a3e6f1f1f471c1cb79aa538323d6a9222d2706b5
Laney <laneyluong@gmail.com>
2022-02-06 19:08:21 -0800

    update userAuth components and route util

---
+++ frontend/src/components/session/login_from.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+      errors: {}
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.renderErrors = this.renderErrors.bind(this);
+  }
+
+  componentDidUpdate() {
+    if (this.props.currentUser === true) this.props.history.push('/');
+    this.setState({ errors: this.props.errors })
+  }
+
+  update(field) {
+    return e => this.setState({ [field]: e.currentTarget.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.login(this.state);
+  }
+
+  renderErrors() {
+    return (
+      <ul>
+        {Object.keys(this.state.errors).map((error, idx) => (
+          <li key={idx}> {this.state.errors[error]} </li>
+        ))}
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <input type="text"
+              value={this.state.email}
+              onChange={this.update('email')}
+              placeholder="Email"
+            />
+            <br />
+            <input type="password"
+              value={this.state.password}
+              onChange={this.update('password')}
+              placeholder="Password"
+            />
+            <br />
+            <input type="submit" value="Submit" />
+            {this.renderErrors()}
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default withRouter(LoginForm);
