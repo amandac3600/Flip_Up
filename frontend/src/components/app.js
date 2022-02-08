163ebf79e3b4f62d3d3ee86d6977b1db0e36f910
amandac3600 <amandac3600@gmail.com>
2022-02-07 18:06:13 -0800

    add route

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
-import Splash from '../splash/splash'
+import Splash from './splash/splash'
 import LoginFormContainer from './session/login_form_container'
 import SignupFormContainer from './session/signup_form_container'
 
