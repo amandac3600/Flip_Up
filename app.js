9892f7bcc0046b22b4d74bb1d8fbebc795678e2e
alex-ciminillo <ciminilloa@findlay.edu>
2022-02-09 12:16:25 -0500

    Heroku setup

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
+
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
