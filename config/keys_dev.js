a8daa7aa9acee615a3824234f1f2e394f6dca04e
HelenEdwards <hgedwards87@gmail.com>
2022-02-09 18:03:35 -0800

    changing heroku stuff

---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://dev:TnyBuePTElY6wuuE@cluster0.6z97s.mongodb.net/FlipUp?retryWrites=true&w=majority',
+    secretOrKey: '27AF9FD6BC8DAF8CDF91AF52AA5BD'
+  }
