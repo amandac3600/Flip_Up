6b5cd3ae4b6ec099ad1145b2ec457543848988a2
amandac3600 <amandac3600@gmail.com>
2022-02-10 18:18:24 -0800

    fix nav bar bug

--- frontend/src/components/nav/nav_container.jsx
+++ frontend/src/components/nav/nav_container.jsx
@@ -2,14 +2,15 @@ import { connect } from 'react-redux';
 import { logout, fetchCurrentUser } from '../../actions/session_actions';
 import Nav from './nav'
 
-const mapStateToProps = (state) => ({
-  // currentUser: state.entities.session.currentUserId,
-  currentUser: state.session.user,
-})
+const mapStateToProps = (state) => {
+  return ({
+    // currentUser: state.entities.session.currentUserId,
+    currentUser: state.session.user,
+  })
+}
 
 const mapDispatchToProps = dispatch => ({
-  logout: () => dispatch(logout()),
-  fetchCurrentUser: () => dispatch(fetchCurrentUser()),
+  logout: () => dispatch(logout())
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Nav)
