2b265fbb62078b822f7dd819548288f7c599d165
Laney <laneyluong@gmail.com>
2022-02-11 20:46:24 -0800

    update edit user form

--- frontend/src/components/user_profile/update_user_form.jsx
+++ frontend/src/components/user_profile/update_user_form.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import NavContainer from '../nav/nav_container';
+import './update_user_form.css';
 
 class UpdateUserForm extends React.Component {
   constructor(props) {
@@ -53,39 +55,44 @@ class UpdateUserForm extends React.Component {
   }
 
   render() {
-    console.log(this.state)
+
     if (!this.props.user || !this.state.username) return null;
     return (
-      <div className="signup-form-container">
-        <form onSubmit={this.usernameSubmit}>
-          <div className="signup-form">
-
+      <div className="update-user-form-container">
+        <NavContainer />
+        <form onSubmit={this.usernameSubmit} className='update-user-form'>
+          <div className="update-user-form-items">
+            <div className='update-user-form-title'>Edit User Form</div>
             <br />
-            <input type="text"
+            <label >Username:
+              <br /><input type="text"
               value={this.state.username}
               onChange={this.update('username')}
               placeholder="username"
-            />
+              /></label>
             <br />
+            <label >Email: <br/>
             <input type="text"
               value={this.state.email}
               onChange={this.update('email')}
               placeholder="email"
-            />
+            /></label>
             <br />
+            <label >New Password:<br />
             <input type="password"
               value={this.state.password}
               onChange={this.update('password')}
               placeholder="New Password"
-            />
+            /></label>
             <br />
+            <label >Confirm New Password:<br />
             <input type="password"
               value={this.state.password2}
               onChange={this.update('password2')}
-              placeholder="Confirm New Password"
-            />
+                placeholder="Confirm New Password"
+            /></label>
             <br />
-            <input type="submit" value="Submit" />
+            <input type="submit" value="Update User" />
             {this.renderErrors()}
           </div>
         </form>
